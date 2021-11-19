5014355cf3f02da97eb65cd34a19430bbad3ab7a
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-11-19 15:44:42 -0300

    Created repository Coupon

---
+++ couponservice/src/main/java/com/cloud/couponservice/repository/CouponRepository.java
@@ -0,0 +1,8 @@
+package com.cloud.couponservice.repository;
+
+import com.cloud.couponservice.model.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CouponRepository extends JpaRepository<Coupon, Long> {
+    
+}
