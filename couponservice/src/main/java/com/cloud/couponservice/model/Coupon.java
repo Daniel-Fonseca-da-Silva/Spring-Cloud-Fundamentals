e963c5d88dd5495be439f63a14e9478d37465fe7
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-11-19 16:21:13 -0300

    Removed the h2 and tested with new parameters model

--- couponservice/src/main/java/com/cloud/couponservice/model/Coupon.java
+++ couponservice/src/main/java/com/cloud/couponservice/model/Coupon.java
@@ -6,7 +6,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.math.BigDecimal;
 
 @Entity
 @Data
@@ -15,9 +14,8 @@ public class Coupon {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    private String description;
-    private BigDecimal price;
+    private String code;
+    private String discount;
     private String expDate;
 
 }
