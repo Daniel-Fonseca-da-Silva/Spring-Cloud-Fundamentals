5c4f65ca0b933bbbb99679b6807ec902ed09efc2
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-11-19 15:41:26 -0300

    Created model Coupon

---
+++ couponservice/src/main/java/com/cloud/couponservice/model/Coupon.java
@@ -0,0 +1,23 @@
+package com.cloud.couponservice.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+public class Coupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private String expDate;
+
+}
