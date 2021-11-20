95c4033d2e4b300a3de7b2b694a21114aa465837
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-11-19 21:47:57 -0300

    Created repository Product

---
+++ productservice/src/main/java/com/cloud/productservice/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.cloud.productservice.repository;
+
+import com.cloud.productservice.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
