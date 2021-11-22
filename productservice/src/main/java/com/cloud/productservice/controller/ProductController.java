01507731e0be4501d1c6e016f16e0a6670012925
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-11-22 12:10:28 -0300

    Use the client

--- productservice/src/main/java/com/cloud/productservice/controller/ProductController.java
+++ productservice/src/main/java/com/cloud/productservice/controller/ProductController.java
@@ -1,22 +1,31 @@
 package com.cloud.productservice.controller;
 
+import com.cloud.productservice.model.Coupon;
 import com.cloud.productservice.model.Product;
 import com.cloud.productservice.repository.ProductRepository;
+import com.cloud.productservice.restclients.CouponClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+
 @RestController
 @RequestMapping("/product")
 public class ProductController {
+
+    @Autowired
+    CouponClient couponClient;
 
     @Autowired
     ProductRepository repo;
 
     @RequestMapping(value = "/products", method = RequestMethod.POST)
     public Product createProduct(@RequestBody Product product) {
+        Coupon coupon = couponClient.getCoupon(product.getCouponCode());
+        product.setPrice(product.getPrice().subtract(BigDecimal.valueOf(Double.parseDouble(coupon.getDiscount()))));
         return repo.save(product);
     }
 
--- productservice/src/main/java/com/cloud/productservice/model/Product.java
+++ productservice/src/main/java/com/cloud/productservice/model/Product.java
@@ -2,10 +2,7 @@ package com.cloud.productservice.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Entity
@@ -18,5 +15,7 @@ public class Product {
     private String name;
     private String description;
     private BigDecimal price;
+    @Transient
+    private String couponCode;
 
 }
